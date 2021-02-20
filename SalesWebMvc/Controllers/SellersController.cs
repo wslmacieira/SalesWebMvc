6d78ff92c522d813fbebcf16e73a960f611d0817
wslmacieiradev <wslmacieira@outlook.com>
2021-02-20 15:09:55 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,20 @@ namespace SalesWebMvc.Controllers
             var list = _service.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)      
+        {
+            _service.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
